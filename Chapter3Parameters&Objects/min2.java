08c8226880e36225ff23e67074d79e5160c1b8de
Hawa Drammeh <hawad416@gmail.com>
2021-05-27 16:13:56 -0700

    Create min2.java

---
+++ Chapter3Parameters&Objects/min2.java
@@ -0,0 +1,15 @@
+//returns smallest of the three values using MAth.min
+
+public static int min (int a, int b, int c) {
+		
+		if(a<b) {
+			return Math.min(a, c);
+		
+		}else {
+			return Math.min(b, c);
+		}
+		
+	
+		
+	
+	}
