b29f48045442e7abc0d59464eca7e5f218857157
Hawa Ali Drammeh <hawad416@gmail.com>
2021-07-12 05:36:23 -0700

    Create oddsArray.java

---
+++ ArraysChapter7/oddsArray.java
@@ -0,0 +1,13 @@
+/*Write code that creates an array named odds and stores all odd numbers between -6 and 38 into 
+it using a for loop. Make the array's size exactly large enough to store the numbers.
+*/
+
+ int [] odds = new int[22];
+2
+        
+3
+        for(int i = 0; i<22; i++) {
+4
+            odds[i] = i * 2 -5;
+5
+        }
