e6161c22afe81c8a6eae069a64ebe0236de4ba6b
Hawa Ali Drammeh <hawad416@gmail.com>
2021-11-27 03:32:42 -0800

    Update repeat.java

--- RecursionChapter12/repeat.java
+++ RecursionChapter12/repeat.java
@@ -1,3 +1,19 @@
+  //expensive operation
+
+
+public static String repeat(String s, int n ){
+            
+        if (n < 0 ) throw new IllegalArgumentException();
+        
+        if(n == 0) return "";
+        
+        return s + repeat(s, n-1);
+        
+    }
+
+
+//less expensive operation
+
 public String repeat(String str, int n) {
     if(n < 0)
         throw new IllegalArgumentException();
