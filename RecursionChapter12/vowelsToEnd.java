9b639414d9c4898559b0c44a93d566242076f8ea
Hawa Ali Drammeh <hawad416@gmail.com>
2021-11-27 17:16:38 -0800

    Update vowelsToEnd.java

--- RecursionChapter12/vowelsToEnd.java
+++ RecursionChapter12/vowelsToEnd.java
@@ -5,32 +5,32 @@
 //For example, the call of vowelsToEnd("beautifully") should return "btfllyuiuae".
 
 	public static String vowelsToEnd(String input) {
-      if(input == null) throw new IllegalArgumentException("null string :(");
+	      if(input == null) throw new IllegalArgumentException("null string :(");
 
-      if(input.length() == 1) return input;
+	      if(input.length() == 1) return input;
 
-      String result1 = "";
-      String result2 = "";
+	      String result1 = "";
+	      String result2 = "";
 
-      return vowelsToEnd(input, result1, result2);
+	      return vowelsToEnd(input, result1, result2);
 	}
 	
 
 //method that constructs 2 different strings depending on if a character is a vowel or not 
 	private static String vowelsToEnd(String input, String result1, String result2) {
 		
-      if(input == null) throw new IllegalArgumentException("Null String :(");
+	      if(input == null) throw new IllegalArgumentException("Null String :(");
 
-    // returns the first nonwoels string to the reversed vowel string!
-      if(input.length() == 0) return result1 + result2;
+	    // returns the first nonwoels string to the reversed vowel string!
+	      if(input.length() == 0) return result1 + result2;
 
-      if(isVowel(input.charAt(0))) {
-        result2 = input.charAt(0) + result2;
-        return vowelsToEnd(input.substring(1), result1, result2);
-      }else {
-        result1+=input.charAt(0);
-        return vowelsToEnd(input.substring(1), result1, result2);
-      }
+	      if(isVowel(input.charAt(0))) {
+		result2 = input.charAt(0) + result2;
+		return vowelsToEnd(input.substring(1), result1, result2);
+	      }else {
+		result1+=input.charAt(0);
+		return vowelsToEnd(input.substring(1), result1, result2);
+	      }
 
 
 	}
@@ -39,12 +39,12 @@
 	
 	private static boolean isVowel(char n) {
 		
-      if(n == 'a' ||
-         n == 'e' ||
-         n == 'i' ||
-         n == 'o' ||
-         n == 'u') return true;
+	      if(n == 'a' ||
+		 n == 'e' ||
+		 n == 'i' ||
+		 n == 'o' ||
+		 n == 'u') return true;
 
-      return false;
+	      return false;
 	}
 	
